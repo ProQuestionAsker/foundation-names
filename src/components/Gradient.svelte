42a2d4401a5a3706ca97b125e856c0c28d20ae1c
Amber Thomas <ProQuestionAsker@gmail.com>
2021-01-29 15:21:31 -0800

    cleaning up gradient file

--- src/components/Gradient.svelte
+++ src/components/Gradient.svelte
@@ -1,23 +1,16 @@
 <script>
-    import { flatten, scaleCanvas } from 'layercake';
-    import { scaleLinear } from 'd3-scale';
+    import { scaleCanvas } from 'layercake';
 
     // Import the getContext function from svelte
     import { getContext } from 'svelte';
     import { hsl } from 'd3-color';
-    import { quantile, ascending } from 'd3-array';
-    import {interpolate, interpolateHslLong} from 'd3-interpolate'
-
-    const margins = {
-        top: 10,
-        left: 20,
-        right: 20, 
-        bottom: 20
-    }
+    import { interpolateHslLong} from 'd3-interpolate'
+    
+    const marginTop = 15;
 
     // Access the context using the 'LayerCake' keyword
     // Grab some helpful functions
-    const { data, xGet, yGet, width, height } = getContext('LayerCake');
+    const { width, height } = getContext('LayerCake');
     const { ctx } = getContext('canvas');
     const startColor = hsl(19.5, 0.46, 0.15)
     const endColor = hsl(30, 0.5, 0.99)
@@ -25,23 +18,7 @@
 
     const n = 512;
 
-    // export let block = 'off'
-    // export let filterProp;
-    // export let filterValue;
     export let step; 
-
-        // if data needs to be filtered, filter it
-    //     let filteredData = $data;
-    // $: if (filterProp && filterValue) {
-    //     filteredData = $data.filter(d => d[filterProp] === filterValue)
-    // }
-
-    // $: flatLight = filteredData.map(d => +d.lightness).sort((a, b) => ascending(a, b))
-
-
-    // $: firstQuant = quantile(flatLight, 0.1)
-    // $: lastQuant = quantile(flatLight, 0.9)
-    // $: lightnessScale = scaleLinear().range([margins.left, $width - margins.right]).domain([0.15, 0.99])
 
     $: {
         if ($ctx) {
@@ -49,16 +26,12 @@
             $ctx.clearRect(0, 0, $width, $height)
 
             if (step !== 'all') {
-                $ctx.canvas.style.width = `calc(100% - ${margins.right + margins.left}px)`;
-                $ctx.canvas.style.marginTop = `${margins.top}px`;
+                $ctx.canvas.style.width = `${$width}px`;
+                $ctx.canvas.style.marginTop = `${marginTop}px`;
                 $ctx.canvas.style.height = "20px";
                 $ctx.canvas.style.imageRendering = "pixelated";
 
                 // draw rectangle
-                // $ctx.beginPath();
-                // $ctx.rect(margins.top, margins.left, $width - margins.left - margins.right, 50);
-                // $ctx.stroke();
-
                 for (let i = 0; i < n; ++i) {
                     $ctx.fillStyle = colors(i / (n - 1));
                     $ctx.fillRect(i, 0, 1, 1);
@@ -66,25 +39,6 @@
 
             }
 
-            
-
-
-            // for (let i = 0; i < n; ++i) {
-            //     $ctx.fillStyle = colors(i / (n - 1));
-            //     $ctx.fillRect(i, 0, 1, 1);
-            // }
-
-            // if (block === 'on'){
-            //     console.log({check: lightnessScale(firstQuant), check2: lightnessScale(lastQuant)})
-            //     // draw 80% rectangle
-            //     //$ctx.beginPath();
-            //     $ctx.lineWidth = 1;
-            //     $ctx.strokeRect(lightnessScale(firstQuant), 0 , lightnessScale(lastQuant - firstQuant), 20);
-            //     // $ctx.stroke();
-            // }
-
-
-            
         }
     }
 
