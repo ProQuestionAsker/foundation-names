62c1838cadfc8c4fc27a79cbae2b397082e98498
Amber Thomas <ProQuestionAsker@gmail.com>
2021-02-17 17:59:14 -0800

    tooltips keyboard triggerable

--- src/components/Controller.svelte
+++ src/components/Controller.svelte
@@ -1,6 +1,7 @@
 <script>
     import TooltipDisplay from "./TooltipDisplay.svelte"
     import { getContext, onMount } from 'svelte';
+import Explore from "./Explore.svelte";
 
 
     export let blockDimensions;
@@ -10,7 +11,11 @@
     export let controllerContainer;
 
     let groupSelOutline;
+    let currentIndex;
     let mount = false
+    let found;
+    let exploreSwatches = false;
+    let ind = 0
 
     // onMount(() => {
     //     console.log({groupSelOutline})
@@ -34,20 +39,46 @@
     $: moveFocusGroup(currentGroup)
 
     function handleKeyPress(event){
-        console.log({shift: event.shiftKey, tab: event.key === 'Tab'})
-        if (event.key === 'Shift' && event.key === 'Tab' || event.key === 'Escape') controllerContainer.focus();
+        const key = event.key
+        console.log({key})
+        if (key === 'Shift' && key === 'Tab' || key === 'Escape') {
+            exploreSwatches = false;
+            controllerContainer.focus();
+        }
+        if (key === 'ArrowUp' || key === 'ArrowDown'){
+            exploreSwatches = true;
+            const swatches = groupedData[currentGroup][1]
+            const total = swatches.length - 1
+            ind = ind > total ? total : ind
+            console.log({swatches, total})
+            if (key === 'ArrowUp'){
+                const newIndex = ind === total ? 0 : ind + 1
+                ind = newIndex 
+                console.log({ind, newIndex})
+                found = swatches[newIndex]
+            } else {
+                const newIndex = ind === 0 ? total : ind - 1
+                ind = newIndex
+                console.log({ind, newIndex})
+                found = swatches[newIndex]
+            }
+        }
+        else exploreSwatches = false
     }
 
 
 </script>
 
 {#if options.includes('histogram')}
-<div bind:this={groupSelOutline}
-    on:keydown|preventDefault={handleKeyPress}
-    tabindex=-1
-    class='group-select'    
-    style="width:{blockWidth + (blockPadding * 2)}px; height:{groupedData[currentGroup][1].length * (blockHeight + blockPadding) + blockPadding}px; left:{$xScale(groupedData[currentGroup][0])}px"
-></div>
+    <div bind:this={groupSelOutline}
+        on:keydown|preventDefault={handleKeyPress}
+        tabindex=-1
+        class='group-select'    
+        style="width:{blockWidth + (blockPadding * 2)}px; height:{groupedData[currentGroup][1].length * (blockHeight + blockPadding) + blockPadding}px; left:{$xScale(groupedData[currentGroup][0])}px"
+    ></div>
+    {#if exploreSwatches === true}
+        <TooltipDisplay selected={found} width={$width} {blockDimensions} height={$height} xScale={$xScale} />
+    {/if}
 {/if}
 
 
