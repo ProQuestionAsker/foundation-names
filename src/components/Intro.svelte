658bc66c4dcd1e1ddd7a9d10a911428c90ccc4e2
Amber Thomas <ProQuestionAsker@gmail.com>
2021-03-05 10:21:22 -0800

    adding byline and date

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -8,6 +8,15 @@ import copy from "../data/copy.json";
         <h2 class='dek'>{copy.dek}</h2>
     </div>
 
+    <div class='meta'>
+        <div class='byline-container'>
+            <p class='byline'>{@html copy.storyBy}</p>
+            <p class='byline'>{@html copy.codeBy}</p>
+        </div>
+
+        <time>{copy.date}</time>
+    </div>
+
 
     {#each copy.intro as {type, value}}
         <p class='prose'>{@html value}</p>
@@ -30,19 +39,52 @@ section {
     font-size: 100px;
     text-align: center;
     line-height: 1.2;
+    margin-top: 2rem;
     margin-bottom: 0;
 }
 .dek {
-    font-size: 32px;
+    font-size: 28px;
     text-transform: uppercase;
     color: var(--gray);
     font-family: 'National 2 Narrow Web';
     font-weight: 200;
     text-align: center;
-    margin-top: 0.25em;
+    margin: 0.25em auto 0;
+    max-width: 22rem;
+    line-height: 1;
 }
 
-.header {
-    margin-bottom: 4rem;
+
+.meta {
+    margin: 0 auto 3rem;
+    color: var(--gray-dark);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 30rem;
+    font-family: 'National 2 Narrow Web'
+}
+
+
+.byline-container {
+    color: var(--gray-dark);
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+    margin-top: 2rem;
+}
+
+.byline {
+    margin: 0.1rem;
+    color: var(--gray);
+}
+
+time {
+    text-align: center;
+    margin: 0.75rem auto 0;
+    color: var(--gray);
 }
 </style>
