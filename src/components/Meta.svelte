f18674a1c3d9b41f79df35e0393e1a66088aa742
Amber Thomas <ProQuestionAsker@gmail.com>
2021-03-22 17:19:33 -0700

    updating metadata

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <svelte:head>
-  <title>Svelte Starter</title>
+  <title>{copy.title}</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta name="description" content="{copy.dek}" />
-  <meta name="news_keywords" content="" />
+  <meta name="news_keywords" content="foundation, sephora, ulta, names, lightness" />
 
   <meta property="og:title" content="{copy.title}" />
   <meta property="og:site_name" content="The Pudding" />
@@ -17,19 +17,19 @@
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
 
-  <meta property="og:image" content="" />
+  <meta property="og:image" content="https://pudding.cool/2021/03/foundation-names/assets/social/social-facebook.png" />
   <meta property="og:image:type" content="image/jpeg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="600" />
 
   <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="" />
-  <meta name="twitter:creator" content="" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:image:src" content="" />
+  <meta name="twitter:site" content="https://pudding.cool" />
+  <meta name="twitter:creator" content="The Pudding" />
+  <meta name="twitter:title" content="{copy.title}" />
+  <meta name="twitter:description" content="{copy.dek}" />
+  <meta name="twitter:image:src" content="https://pudding.cool/2021/03/foundation-names/assets/social/social-twitter.png" />
 
   <meta name="robots" content="max-image-preview:large" />
 
-  <link rel="canonical" href="" />
+  <link rel="canonical" href="https://pudding.cool/2021/03/foundation-names" />
 </svelte:head>
